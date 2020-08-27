539c9a0dc96bea2dcafd8850c6cee19a2c683467
Neeraj Kumar <nrjkumar1@outlook.com>
2020-08-27 14:21:15 +0530

    Good mobile version

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb+srv://admin-neeraj:toughPassword@cluster0.mxn2b.mongodb.net/todolistDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = new mongoose.Schema({
   name: String
